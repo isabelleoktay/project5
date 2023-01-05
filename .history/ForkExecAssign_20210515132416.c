3b148a88d3f7ed80696b09d9e1ea2bcd64f7c1e4
Isabelle Oktay <iso212@nyu.edu>
2023-01-05 17:47:44 -0500

    fixed description

--- .history/ForkExecAssign_20210515132416.c
+++ .history/ForkExecAssign_20210515132416.c
@@ -1,19 +1,20 @@
 /* Author: Isabelle Oktay
+ * Version: 05.15.2021
  * Description: This program randomizes numbers between 1 and 10 and prompts the user to add,
                 subtract, or multiply two of these random numbers. The user will input a
                 specific number of questions they want to answer. The user is expected to answer
-                each question within 5 seconds. If not, then the program will move prompt the user to
+                each question within 5 seconds. If not, then the program will move prompt the user to 
                 move on to the next question. At the end of the program, the program will display
-                the number of questions missed, incorrect, and correct.
+                the number of questions missed, incorrect, and correct. 
  */
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <stdio.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <errno.h> 
+#include <stdio.h> 
 #include <sys/wait.h>
 #include <signal.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
@@ -33,17 +34,15 @@ bool miss = false;
  * Return value:
  *  Void
  *
- */
-void missed_answer(int sig)
-{
-    miss = true;
+*/
+void missed_answer(int sig) {
+    miss = true; 
     // user can't input any more answers, will have to prompt to move on
-    printf("enter 0 to continue\n");
-    kill(pid, SIGKILL); // we need to kill the current process to then start again.
+    printf("enter 0 to continue\n"); 
+    kill(pid, SIGKILL); // we need to kill the current process to then start again. 
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     signal(SIGINT, missed_answer);
     int child_status;
@@ -53,10 +52,10 @@ int main(int argc, char **argv)
 
     // prompt the user to input the number of questions they want to answer
     int num = 0;
-    printf("How many questions would you like to answer? ");
+    printf("How many questions would you like to answer? " );
     scanf(" %d", &num);
     printf("\nAwesome! Let's goooo...\n\n");
-
+    
     // create random numbers based on the current time seed
     srand(time(0));
     int op;
@@ -64,117 +63,104 @@ int main(int argc, char **argv)
     int num2;
 
     // iterate for the number of questions
-    for (i = 0; i < num; i++)
-    {
+    for (i = 0; i < num; i++) {
         char answer[20];
         int intAns;
 
         // create a separate timer process for each child
-        pid = fork();
-        if (pid == 0)
-        {
-            pid = execve("Process6P1", NULL, NULL);
-        }
+    	pid = fork();
+        if (pid == 0) {
+            pid = execve("Process6P1",NULL, NULL);
+    	}
 
         // determine which numbers and operator will be used
         op = rand() % 3;
         num1 = rand() % 10;
         num2 = rand() % 10;
-        miss = false;
-
+	    miss = false;
+	
+	
         // addition operation
-        if (op == 0)
-        {
-            printf("What is %d + %d? ", num1, num2);
-            scanf(" %s", answer);
-        }
+    	if(op == 0){
+    	    printf("What is %d + %d? ", num1, num2);
+                scanf(" %s", answer);
+    	}
         // subtraction operation
-        else if (op == 1)
-        {
-            printf("What is %d - %d? ", num1, num2);
-            scanf(" %s", answer);
-        }
+    	else if(op == 1){
+    	    printf("What is %d - %d? ", num1, num2);
+                scanf(" %s", answer);
+    	}
         // multiplication operation
-        else
-        {
-            printf("What is %d * %d? ", num1, num2);
-            scanf(" %s", answer);
-        }
-        intAns = atoi(answer); // convert to int
-
-        // now we need to check if the answers are correct, incorrect, or missed
-        if (op == 0)
-        {
+    	else{
+    	    printf("What is %d * %d? ", num1, num2);
+                scanf(" %s", answer);
+    	}
+    	intAns = atoi(answer); // convert to int
+	
+	    // now we need to check if the answers are correct, incorrect, or missed
+        if (op == 0) {
 
             // each of these clauses depend on if miss is true or false;
             // the value of miss depends on the timer
-            if (num1 + num2 != intAns && !miss)
-            {
+            if (num1 + num2 != intAns && !miss) {
                 incorrect++;
                 printf("\n");
-            }
-            else if (num1 + num2 == intAns && !miss)
-            {
+            } 
+            else if (num1 + num2 == intAns && !miss){
                 correct++;
                 printf("\n");
             }
-            else
-            {
+            else{
                 printf("\n");
                 miss = false;
                 missed++;
-            }
-        }
-        else if (op == 1)
-        {
+            } 
 
-            if (num1 - num2 != intAns && !miss)
-            {
-                incorrect++;
-                printf("\n");
-            }
-            else if (num1 - num2 == intAns && !miss)
-            {
-                correct++;
-                printf("\n");
-            }
-            else
-            {
-                printf("\n");
-                missed++;
-                miss = false;
-            }
-        }
-        else
-        {
+        } else if (op == 1) {
 
-            if (num1 * num2 != intAns && !miss)
-            {
+            if (num1 - num2 != intAns && !miss) {
                 incorrect++;
                 printf("\n");
-            }
-            else if (num1 * num2 == intAns && !miss)
-            {
+            } 
+            else if(num1 - num2 == intAns && !miss){
                 correct++;
                 printf("\n");
             }
-            else
-            {
+            else {
                 printf("\n");
                 missed++;
                 miss = false;
+            } 
+
+        } else {
+
+            if (num1 * num2 != intAns && !miss) {
+                incorrect++;
+                printf("\n");
+            } 
+            else if (num1 * num2 == intAns && !miss){
+                correct++;
+                printf("\n");
             }
+            else {
+                printf("\n");
+                missed++;
+                miss = false;
+            } 
         }
 
         // kill the child timer process
         kill(pid, SIGKILL);
     }
 
+
     // print out results
     printf("Your total score is: \n\n");
     printf("%d correct answers\n%d incorrect answers\n%d missed questions\n",
            correct, incorrect, missed);
 
-    return (0);
+    return(0);
     exit(0); // exit parent process
 }
+
+
